73f402ae0cb814644f4bbe77d6ddee0ea1728d55
richard <richard@androidminds.cn>
2017-11-30 16:59:18 +0800

    add a web filter

--- src/main/java/cn/androidminds/springstudy/SpringstudyApplication.java
+++ src/main/java/cn/androidminds/springstudy/SpringstudyApplication.java
@@ -2,8 +2,10 @@ package cn.androidminds.springstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringstudyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringstudyApplication.class, args);
