0b63eec54f587117fe0fba6f53d43802f7081002
richard <richard@androidminds.cn>
2017-11-30 17:52:36 +0800

    add upload file function

--- src/main/java/cn/androidminds/springstudy/web/LoginController.java
+++ src/main/java/cn/androidminds/springstudy/web/LoginController.java
@@ -7,7 +7,12 @@ import cn.androidminds.springstudy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.*;
 import java.util.Date;
 
 
@@ -17,6 +22,27 @@ public class LoginController {
 
     @RequestMapping(value = "/")
     public String loginPage(){return "login";}
+
+    @RequestMapping(value = "/upload_file")
+     public @ResponseBody String handleFileUpload(@RequestParam("file")MultipartFile file){
+        if(!file.isEmpty()){
+            try {
+                BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(new File(file.getOriginalFilename())));
+                out.write(file.getBytes());
+                out.flush();
+                out.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                return"上传失败,"+e.getMessage();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return"上传失败,"+e.getMessage();
+            }
+            return"上传成功";
+        }else{
+            return"上传失败，因为文件是空的.";
+        }
+    }
 
     @RequestMapping(value = "/register.html")
     public String registerPage(){
